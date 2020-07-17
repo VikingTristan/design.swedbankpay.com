e4c204d81ddf0e134a3a515aa4c6b4bb702ed6a5
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-07-17 14:50:51 +0200

    Update variables script

--- .github/scripts/variables.sh
+++ .github/scripts/variables.sh
@@ -1,11 +1,13 @@
 if [ "$BRAND" == "payex" ]; then
-    echo ::set-env name=BRAND_URL::https://payexdesignguide.z16.web.core.windows.net
-    echo ::set-env name=BRAND_BUILD_SCRIPT::build:prod:payex
-    echo ::set-output name=BRAND_BUILD_SCRIPT::build:prod:payex
+    echo ::set-output name=BRAND_NAME::PayEx
+    echo ::set-output name=BRAND_URL::https://payexdesignguide.z6.web.core.windows.net
+    echo ::set-output name=AZURE_ACCOUNT::payexdesignguide
+    echo ::set-output name=BUILD_SCRIPT::build:prod:payex
 elif [ "$BRAND" == "swedbankpay" ]; then
-    echo ::set-env name=BRAND_URL::https://swedbankpaydesignguide.z16.web.core.windows.net
-    echo ::set-env name=BRAND_BUILD_SCRIPT::build:prod
-    echo ::set-output name=BRAND_BUILD_SCRIPT::build:prod
+    echo ::set-output name=BRAND_NAME::SwedbankPay
+    echo ::set-output name=BRAND_URL::https://swedpaydesignguide.z6.web.core.windows.net
+    echo ::set-output name=AZURE_ACCOUNT::swedpaydesignguide
+    echo ::set-output name=BUILD_SCRIPT::build:prod
 else
     echo "Unknown brand!"
 fi

c78f54793df58c16cc61c3dea32aaf508cfde60d
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-07-17 15:31:37 +0200

    fix swedbankpay azure account

--- .github/scripts/variables.sh
+++ .github/scripts/variables.sh
@@ -6,7 +6,7 @@ if [ "$BRAND" == "payex" ]; then
 elif [ "$BRAND" == "swedbankpay" ]; then
     echo ::set-output name=BRAND_NAME::SwedbankPay
     echo ::set-output name=BRAND_URL::https://swedpaydesignguide.z6.web.core.windows.net
-    echo ::set-output name=AZURE_ACCOUNT::swedpaydesignguide
+    echo ::set-output name=AZURE_ACCOUNT::swedbankpaydesignguide
     echo ::set-output name=BUILD_SCRIPT::build:prod
 else
     echo "Unknown brand!"

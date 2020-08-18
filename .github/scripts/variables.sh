cc98512dcca03495b6152419034bdc6b052927f8
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-08-18 15:28:49 +0200

    Fix swedbank pay variable url

--- .github/scripts/variables.sh
+++ .github/scripts/variables.sh
@@ -5,7 +5,7 @@ if [ "$BRAND" == "payex" ]; then
     echo ::set-output name=BUILD_SCRIPT::build:prod:payex
 elif [ "$BRAND" == "swedbankpay" ]; then
     echo ::set-output name=BRAND_NAME::SwedbankPay
-    echo ::set-output name=BRAND_URL::https://swedpaydesignguide.z6.web.core.windows.net
+    echo ::set-output name=BRAND_URL::https://swedbankpaydesignguide.z6.web.core.windows.net
     echo ::set-output name=AZURE_ACCOUNT::swedbankpaydesignguide
     echo ::set-output name=BUILD_SCRIPT::build:prod
 else
